649af5f827f623295f2966704cb18667941d3305
e.an <55692615+dennislee928@users.noreply.github.com>
2024-05-22 16:51:29 +0800

    start

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-     
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+  
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
